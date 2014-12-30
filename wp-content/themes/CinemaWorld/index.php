a745263e907fd2e3915147b89a40b583afe9eac4
Umer Farooq <mail.to.umerfarooq@gmail.com>
2014-12-30 15:36:00 +0500

    Search enhanced to load up Movie details

--- wp-content/themes/CinemaWorld/index.php
+++ wp-content/themes/CinemaWorld/index.php
@@ -33,7 +33,7 @@
                         </div>
                     </div>
 
-                    <div class='row' style='height: 55%;'>
+                    <div class='row' style='height: 50%;'>
                         <div class='col-lg-12'>
                             <?php echo mb_strimwidth(get_field('movie_synopsis'), 0, 250, "");?>
                         </div>
@@ -41,12 +41,12 @@
 
                     <div class='row'>
                         <div class='col-lg-6'>
-                            <a href='JavaScript:void(0);' onclick="$('#movie_<?php echo get_the_ID(); ?>').popup();">
+                            <a href='JavaScript:void(0);' class="movie_<?php echo get_the_ID(); ?>_open">
                                 <span class='caption-action read' ></span>
                                 <label>Read more</label>
                             </a>
                         </div>
-                        <div class='col-lg-4 col-lg-offset-2'>
+                        <div class='col-lg-5 col-lg-offset-1'>
                             <a href='JavaScript:void(0);' onclick="$('#movie_<?php echo get_the_ID(); ?>').popup();">
                                 <span class='caption-action trailer'  ></span>
                                 <label>Watch trailer</label>
@@ -54,18 +54,54 @@
                         </div>
                     </div>
 
+                    <script type="text/javascript">
+                    jQuery(document).ready(function($){
+                        $("#movie_<?php echo get_the_ID(); ?>").popup();
+                    });                    
+                    </script>
+
                 </div>                   
             </li>
 
             <?php  $id=(string)get_the_ID(); ?>
-            <div class="hide" id="movie_<?php echo $id;?>">
-                <div class="movie-title">Title : <?php the_title();?></div>
-                <div class="movie-image img-responsive"><img src="<?php the_field('movie_image');?>"/></div>
-                <div class="movie-country">Country : <?php the_field('movie_country');?></div>
-                <div class="movie-genre">Genre : <?php the_field('movie_genre');?></div>
-                <div class="movie-cast">Cast / Director : <?php the_field('movie_casts');?></div>
-                <div class="movie-duration">Duration : <?php the_field('movie_duration');?></div>
-                <div class="movie-synopsis">Synopsis : <?php the_field('movie_synopsis');?></div>
+            <div class="popup light" id="movie_<?php echo $id;?>">
+                <div class='row'>
+                    <div class='col-lg-12'>
+                        <h1><?php the_title();?></h1>
+                    </div>
+                </div>
+
+                <div class='row'>
+                    <div class='col-lg-12'>
+                        <img src="<?php the_field('movie_image');?>" class="movie-image img-responsive" /></img>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class='col-lg-2'>Country : </div>
+                    <div class='col-lg-10'><?php the_field('movie_country');?></div>
+                </div>
+
+                <div class="row">
+                    <div class='col-lg-2'>Genre : </div>
+                    <div class='col-lg-10'><?php the_field('movie_genre');?></div>
+                </div>
+
+                <div class="row">
+                    <div class='col-lg-2'>Cast / Director : </div>
+                    <div class='col-lg-10'><?php the_field('movie_casts');?></div>
+                </div>
+
+                <div class="row">
+                    <div class='col-lg-2'>Duration : </div>
+                    <div class='col-lg-10'><?php the_field('movie_duration');?></div>
+                </div>
+
+                <div class="row" style='margin-bottom: 3%'>
+                    <div class='col-lg-2'>Synopsis : </div>
+                    <div class='col-lg-10'><?php the_field('movie_synopsis');?></div>
+                </div>
+                <a href="JavaScript:void(0);" id="popout_cross" class="movie_<?php echo $id;?>_close"></a>
             </div>
         <?php 
         endwhile;
@@ -78,10 +114,10 @@
 
 <nav class="nav navbar-vertical main-nav">
     <ul>
-        <li><a href="#">Sunday See The World</a></li>
-        <li><a href="#">Saturday Festival Picks</a></li>
-        <li><a href="#">Thriller Friday</a></li>
-        <li><a href="#" class="scheduler_open">Schedules</a></li>
+        <li><a href="JavaScript:void(0);" class="sunday_popup_open">Sunday See The World</a></li>
+        <li><a href="JavaScript:void(0);" class="saturday_popup_open">Saturday Festival Picks</a></li>
+        <li><a href="JavaScript:void(0);" class="friday_popup_open">Thriller Friday</a></li>
+        <li><a href="JavaScript:void(0);" class="scheduler_open">Schedules</a></li>
     </ul>
 </nav>
 
