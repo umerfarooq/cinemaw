672b84c437b2dbb80f5eaece85c94949aa2f114b
tahirwaseer <tahirwaseer1@gmail.com>
2015-01-02 02:23:03 +0500

    post expander

--- wp-content/themes/CinemaWorld/bootstrap/js/functions.js
+++ wp-content/themes/CinemaWorld/bootstrap/js/functions.js
@@ -17,6 +17,39 @@ jQuery(document).ready(function($){
 
   $(".load_post").click( function() {
     var link = $(this);
+    if (link.hasClass('static-page')) {
+     $.post(link.attr("href"), {
+          static_page: 1
+        }, function(data) {
+          // link.parents(".entry").html($(data));
+           $(".detail").html($(data));
+             $("#movie_detail").popup('show');
+        }
+      ); 
+    }else if (link.hasClass('nav-link')) {
+     $.post(link.attr("href"), {
+          navigation_popup: 1
+        }, function(data) {
+          // link.parents(".entry").html($(data));
+          $(".detail").html($(data));
+          $('#movie_detail').removeClass('light');
+          $('#movie_detail').addClass('navtype');
+          $("#movie_detail").popup('show');
+        }
+      ); 
+    }else if (link.hasClass('date-link')) {
+     $.post(link.attr("href"), {
+          date: link.attr('data-date')
+        }, function(data) {
+          alert('Yayy');
+          sb=$(this).parent().siblings('.selected');
+          sb.removeClass('selected');
+          $(this).parent().addClass('selected');
+
+          $('schedule_timetable').html($(data));
+        }
+      ); 
+    } else {
     $.post(link.attr("href"), {
         post_expander: 1
       }, function(data) {
@@ -24,8 +57,24 @@ jQuery(document).ready(function($){
          $(".detail").html($(data));
            $("#movie_detail").popup('show');
       }
-    );
+    );};
     return false;
+  });
+
+  $('.load-schedule').click(function(){
+    // sb=$(this).parent().siblings('.selected');
+    // sb.removeClass('selected');
+    // $(this).parent().addClass('selected');
+    var link = $(this);
+
+    $.post('', {
+      post_expander: 1
+    }, function(data) {
+          // link.parents(".entry").html($(data));
+          
+        }
+      ); 
+    
   });
 
   $("#thumbnails").owlCarousel();
