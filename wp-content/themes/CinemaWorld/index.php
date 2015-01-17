d4bc7068880be6d08f0fe32d5786bce1112c0952
Umer Farooq <mail.to.umerfarooq@gmail.com>
2015-01-18 00:50:48 +0500

    Added the custom snippet to lazily load the background movie images

--- wp-content/themes/CinemaWorld/index.php
+++ wp-content/themes/CinemaWorld/index.php
@@ -23,75 +23,99 @@
     <div class='col-lg-10 col-lg-offset-1'>
         <div id="thumbnails" class="owl-carousel owl-theme">
             <?php
-                $args = array(
-                    'category_name' => 'home',
-                    'posts_per_page' => -1,
-                    'post_status' => 'publish',
-                    'post_type' => 'post',
-                    'meta_query' => array(
+            $args = array(
+                'category_name' => 'home',
+                'posts_per_page' => -1,
+                'post_status' => 'publish',
+                'post_type' => 'post',
+                'meta_query' => array(
                         'relation' => 'OR', // Optional, defaults to "AND"
                         array(
                             'key' => 'thumbnail_file',
                             'value' => '',
                             'compare' => '!='
-                        ),
+                            ),
                         array(
                             'key' => 'thumbnail_url',
                             'value' => '',
                             'compare' => '!='
+                            )
                         )
-                    )
                 );
                 // The Query
-                $query = new WP_Query( $args );
+            $query = new WP_Query( $args );
                 // Array of Background Images
-                $images = array();
-                $imageIndex = 0;
+            $images = array();
+            $imageIndex = 0;
 
-                if ( $query->have_posts() ) {
+            if ( $query->have_posts() ) {
                 // The Loop
                 while ( $query->have_posts() ) { 
                     $query->the_post();
                     ?>
                     
                     <?php 
-                        $source = get_field('thumbnail_image_source');
-                        if( $source == 'file') {
-                            $quoted_image = "'".get_field('movie_image_file')."'";
-                            $image = get_field('thumbnail_file');                            
-                        } else {
-                            $quoted_image = "'".get_field('movie_image_url')."'"; 
-                            $image = get_field('thumbnail_url');
-                        }   
+                    $source = get_field('thumbnail_image_source');
+                    if( $source == 'file') {
+                        $quoted_image = "'".get_field('movie_image_file')."'";
+                        $image = get_field('thumbnail_file');                            
+                    } else {
+                        $quoted_image = "'".get_field('movie_image_url')."'"; 
+                        $image = get_field('thumbnail_url');
+                    }   
 
-                        $images[] = $quoted_image;                     
+                    $images[] = $quoted_image;                     
                     ?>
                     
                     <div class='item'><a class="thumb" data-id="<?php echo $imageIndex; ?>" data-post-url="<?php the_permalink(); ?>" href="JavaScript:void(0);"><img class="lazyOwl" data-src="<?php echo $image; ?>" /></a></div>
-                <?php 
+                    <?php 
                     $imageIndex++;
-                    }
                 }
+            }
                 // Reset Query
-                wp_reset_postdata();
-                $images = implode(", ", $images);
+            wp_reset_postdata();
+            $images = implode(", ", $images);
             ?>
         </div>
         
         <script type="text/javascript">
-            jQuery(document).ready(function($){
-                $.backstretch([<?php echo $images; ?>], {duration: 40000000});
+        jQuery(document).ready(function($){
 
-                $(window).on("backstretch.after", function (e, instance, index) {
+                // Array of images
+                var MyImageArray = [<?php echo $images; ?>];
+
+                // You can preload the first image
+                var o = MyImageArray[0], u = new Image;
+                u.onerror=function(){
+                    console.log("error with loading image");
+                };
+                u.onload=function(){
+                     // Start the slideshow passing only the first image
+                     $.backstretch(
+                       [MyImageArray[0]], 
+                       { duration: 40000000}
+                       );
+                     // now we can push more images right before they should appear
+                     var slider = $("body").data("backstretch");
+                     var imgCount = 0;
+                     var timer = setInterval(function(){
+                        ++imgCount;
+                        if(imgCount === (MyImageArray.length-1)) clearInterval(timer);
+                        slider.images.push(MyImageArray[imgCount]);
+                    },1000);
+                 };
+                 u.src=o;
+
+                 $(window).on("backstretch.after", function (e, instance, index) {
                     var post_url = $('.item').find('[data-id="'+index+'"]').attr('data-post-url');
 
                     $.post(post_url, {post_caption: 1}, function(data) {
                         $('#custom-caption').html(data);
                     });
                 });
-            });
-        </script>  
-    </div>  
+             });
+    </script>  
+</div>  
 </div>
 
 <?php get_footer(); ?>
